85503dd276c4017694ad91e2cc8e599e5a4624c5
Dan Sheppard <dan@typewriter.org.uk>
2012-08-28 17:07:52 +0100

    Labels for assembler.

--- build/assemble.pl
+++ build/assemble.pl
@@ -22,6 +22,7 @@ while(<DATAFILE>) {
     code => dehex($line[1]),
     type => $line[2],
     extra => $line[3],
+    offset => $line[4],
   };
 }
 close DATAFILE;
@@ -32,8 +33,12 @@ sub word {
   return ($r<<24)|($a<<16)|($b<<8)|$c;
 }
 
+my (@output,@offset,%labels);
+my $offset = 0;
+
 sub convert {
   local $_ = shift;
+  my $instr = shift;
   
   my $orig = $_;
   my $neg = s/^-//;
@@ -43,7 +48,13 @@ sub convert {
   } elsif(/^[0-9]+/) {
     $val = $_+0;
   } else {
-    die "Bad argument '$orig'\n";
+    die "Bad label '$orig'\n" unless defined $labels{$_};
+    my $jump = $labels{$_}-$offset;
+    if($instr->{'offset'} =~ /F(-?\d+)/) {
+      $jump += $1;
+    }
+    $val = $jump;
+    if($val<0) { $val*=-1; $neg=1; }
   }
   if($neg) {
     $val--;
@@ -52,30 +63,49 @@ sub convert {
   return $val;
 }
 
-my @output;
+sub parse {
+  my ($mode) = @_;
 
-open(INFILE,$filename) || die "Cannot read '$filename'\n";
-while(<INFILE>) {
-  my @outline;
-  next if(/^\s*$/ or /^\s*#/);
-  s/^\s+//;
-  s/\s+$//;
-  my $line = $_;
-  my @line = split /\s+/;
-  my $instr = $opcodes{shift @line};
-  die "Unknown opcode $line[0]" unless defined $instr;
-  @line = map { convert $_ } @line;
-  my @args;
-  my $args = 3-(ord($instr->{'type'})-ord('A'));
-  push @args,(shift @line) for (0..$args-1);
-  unshift @args,0 for(1..(3-@args));
-  push @outline,word($instr->{'code'},@args);
-  if($instr->{'extra'} =~ /V/) {
-    push @outline,(shift @line);
+  my $label = '';
+  $offset = 0;
+  open(INFILE,$filename) || die "Cannot read '$filename'\n";
+  while(<INFILE>) {
+    my @outline;
+    next if(/^\s*$/ or /^\s*#/);
+    s/^\s+//;
+    s/\s+$//;
+    s/#.*$//;
+    if(/^\.(.*)/) {
+      $labels{$1} = $offset unless $mode;
+      $label .= "$_ ";
+      next;
+    }
+    my $line = $_;
+    my @line = split /\s+/;
+    my $instr = $opcodes{shift @line};
+    die "Unknown opcode $line[0]" unless defined $instr;
+    @line = map { convert($_,$instr) } @line;
+    my @args;
+    my $args = 3-(ord($instr->{'type'})-ord('A'));
+    push @args,(shift @line) for (0..$args-1);
+    unshift @args,0 for(1..(3-@args));
+    push @outline,word($instr->{'code'},@args);
+    if($instr->{'extra'} =~ /V/) {
+      push @outline,(shift @line);
+    }
+    if($mode) {
+      push @output,[\@outline,"$label$line"];
+    } else {
+      push @offset,length(@outline);
+    }
+    $offset += @outline;
+    $label = '';
   }
-  push @output,[\@outline,$line];
+  close INFILE;
 }
-close INFILE;
+
+parse(0);
+parse(1);
 
 my $outfile = $filename;
 $outfile =~ s/.aa$//;
